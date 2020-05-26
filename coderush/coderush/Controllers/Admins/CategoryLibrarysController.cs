9372ab9b43e3fcd2f763517c1278db3eed1d1c27
openupmta <openupmta99@gmail.com>
2020-05-26 11:07:02 +0700

    tien - category - library

--- coderush/coderush/Controllers/Admins/CategoryLibrarysController.cs
+++ coderush/coderush/Controllers/Admins/CategoryLibrarysController.cs
@@ -10,90 +10,90 @@ using coderush.Models;
 
 namespace coderush.Controllers.Admins
 {
-    public class IntroducesController : Controller
+    public class CategoryLibrarysController : Controller
     {
         private IeltsDBContext db = new IeltsDBContext();
 
-        // GET: Introduces
+        // GET: CategoryLibrarys
         public ActionResult Index()
         {
-            return View(db.introduces.ToList());
+            return View(db.category_librarys.ToList());
         }
 
-        // GET: Introduces/Details/5
+        // GET: CategoryLibrarys/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            introduce introduce = db.introduces.Find(id);
-            if (introduce == null)
+            category_librarys category_librarys = db.category_librarys.Find(id);
+            if (category_librarys == null)
             {
                 return HttpNotFound();
             }
-            return View(introduce);
+            return View(category_librarys);
         }
 
-        // GET: Introduces/Create
+        // GET: CategoryLibrarys/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Introduces/Create
+        // POST: CategoryLibrarys/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "in_id,in_logo,in_address,in_phone,in_email,in_facebook,in_title,in_content,in_created_at,in_updated_at")] introduce introduce)
+        public ActionResult Create([Bind(Include = "ca_id,ca_name,ca_slug,ca_icon,ca_status,ca_created_at,ca_updated_at")] category_librarys category_librarys)
         {
             if (ModelState.IsValid)
             {
-                db.introduces.Add(introduce);
+                db.category_librarys.Add(category_librarys);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(introduce);
+            return View(category_librarys);
         }
 
-        // GET: Introduces/Edit/5
+        // GET: CategoryLibrarys/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            introduce introduce = db.introduces.Find(id);
-            if (introduce == null)
+            category_librarys category_librarys = db.category_librarys.Find(id);
+            if (category_librarys == null)
             {
                 return HttpNotFound();
             }
-            return View(introduce);
+            return View(category_librarys);
         }
 
-        // POST: Introduces/Edit/5
+        // POST: CategoryLibrarys/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "in_id,in_logo,in_address,in_phone,in_email,in_facebook,in_title,in_content,in_created_at,in_updated_at")] introduce introduce)
+        public ActionResult Edit([Bind(Include = "ca_id,ca_name,ca_slug,ca_icon,ca_status,ca_created_at,ca_updated_at")] category_librarys category_librarys)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(introduce).State = EntityState.Modified;
+                db.Entry(category_librarys).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(introduce);
+            return View(category_librarys);
         }
 
-        // GET: Introduces/Delete/5
+        // GET: CategoryLibrarys/Delete/5
         public ActionResult Delete(int? id)
         {
-            introduce introduce = db.introduces.Find(id);
-            db.introduces.Remove(introduce);
+            category_librarys category_librarys = db.category_librarys.Find(id);
+            db.category_librarys.Remove(category_librarys);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
